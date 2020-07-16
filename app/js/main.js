0616334edb7e67d685e6baf9ae134be055ba3a9f
Weadmead <wead758@gmail.com>
2020-07-16 18:48:27 +0300

    added release__products followers__feed facts

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,22 @@
 $(function () {
   $(".rate-star").rateYo({
-    rating: 5,
-    starWidth: "17px",
+    rating: 4.5,
+    starWidth: "15px",
     readOnly: true,
     ratedFill: "#ffc000",
+    spacing: "4px",
   });
 
   $(".featured__product-slider").slick({
     arrows: true,
   });
+
+  $(".followers__slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+  });
+
+  var mixer = mixitup(".release__product-items");
 });
