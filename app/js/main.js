20d449168e7bf7860eb9d477201f3131baf3e728
Weadmead <wead758@gmail.com>
2020-07-31 15:31:35 +0300

    added category page

--- app/js/main.js
+++ app/js/main.js
@@ -17,13 +17,36 @@ $(function () {
     slidesToScroll: 1,
     arrows: true,
   });
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    grid: false,
+    skin: "round",
+  });
+
+  $(".list-btn").on("click", function () {
+    $(".category__item").addClass("category__item-list");
+  });
+  $(".grid-btn").on("click", function () {
+    $(".category__item").removeClass("category__item-list");
+  });
 
   $(".menu__btn").on("click", function () {
     $(".menu__list").slideToggle();
+  });
+  $(".aside__item-btn").on("click", function () {
+    $(".aside__item-list").slideToggle();
+  });
+  $(".aside__filter-btn").on("click", function () {
+    $(".aside__filter-list").slideToggle();
+  });
+  $(".pricing__range").on("click", function () {
+    $(".pricing__range-list").slideToggle();
   });
   $(".header__btn-menu").on("click", function () {
     $(".user__box").toggleClass("active");
   });
 
-  var mixer = mixitup(".release__product-items");
+  var mixer = mixitup(".release__product-items, .category__content");
 });
